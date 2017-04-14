2f3e2f7d6212ed5162295a73c20798c67a60434e
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2017-04-14 14:09:48 +0200

    doc

---
+++ doc.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+
+W matrix for 2 inputs and 4 neurones
+  | w11    w12    b1 |
+  | w21    w22    b2 |
+  | w31    w32    b3 |
+  | w41    w42    b4 |
+
+ Using an input vector (last element is always 1 to use bias)
+  | i1 |
+  | i2 |
+  |  1 |
+
+*/
